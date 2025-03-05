2bd012492e550a1a47be361a9bdd2f8fa9623cb4
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-03-04 23:42:19 -0800

    Extend Prisma client with Prisma Accelerate

--- apps/backend/prisma/customClient.ts
+++ apps/backend/prisma/customClient.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 import { getHashedPassword } from "../src/utils/auth.utils.js";
 
@@ -9,41 +10,43 @@ const prisma = new PrismaClient({
       password: true,
     },
   },
-}).$extends({
-  query: {
-    user: {
-      async $allOperations({ operation, args, query }) {
-        // Generate hashed password before saving to the database.
-        if (
-          (operation === "create" || operation === "update") &&
-          args.data.password &&
-          typeof args.data.password === "string"
-        ) {
-          args.data.password = await getHashedPassword(args.data.password);
-        }
+})
+  .$extends({
+    query: {
+      user: {
+        async $allOperations({ operation, args, query }) {
+          // Generate hashed password before saving to the database.
+          if (
+            (operation === "create" || operation === "update") &&
+            args.data.password &&
+            typeof args.data.password === "string"
+          ) {
+            args.data.password = await getHashedPassword(args.data.password);
+          }
 
-        return query(args);
-      },
-    },
-  },
-  result: {
-    user: {
-      fullName: {
-        needs: { firstName: true, lastName: true },
-        compute(user) {
-          return `${user.firstName} ${user.lastName}`;
-        },
-      },
-
-      isValidPassword: {
-        needs: { password: true },
-        compute(user) {
-          return async (inputPassword: string): Promise<boolean> =>
-            bcrypt.compare(inputPassword, user.password);
+          return query(args);
         },
       },
     },
-  },
-});
+    result: {
+      user: {
+        fullName: {
+          needs: { firstName: true, lastName: true },
+          compute(user) {
+            return `${user.firstName} ${user.lastName}`;
+          },
+        },
+
+        isValidPassword: {
+          needs: { password: true },
+          compute(user) {
+            return async (inputPassword: string): Promise<boolean> =>
+              bcrypt.compare(inputPassword, user.password);
+          },
+        },
+      },
+    },
+  })
+  .$extends(withAccelerate());
 
 export default prisma;

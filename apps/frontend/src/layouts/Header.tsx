46b1804404f5c7b35a8310ff4f776260287e8806
skharat8 <5607079+skharat8@users.noreply.github.com>
2025-03-10 23:16:39 -0700

    Make the user avatar in app header navigate to user profile

--- apps/frontend/src/layouts/Header.tsx
+++ apps/frontend/src/layouts/Header.tsx
@@ -33,10 +33,12 @@ function Header({ showAvatar = false, showBackButton = false }: HeaderProps) {
     <header className="bg-card sticky top-0 z-10 min-w-full max-w-7xl shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-center px-5 py-3">
         {showAvatar && (
-          <Avatar className="shadow-sm">
-            <AvatarImage src={user?.profileImage} />
-            <AvatarFallback>{`${user?.firstName?.at(0)}${user?.lastName?.at(0)}`}</AvatarFallback>
-          </Avatar>
+          <Link to={`/profile`}>
+            <Avatar className="shadow-[inset_0_0_0_1px_hsla(0,0%,0%,.1)]">
+              <AvatarImage src={user?.profileImage} />
+              <AvatarFallback>{`${user?.firstName?.at(0)}${user?.lastName?.at(0)}`}</AvatarFallback>
+            </Avatar>
+          </Link>
         )}
         {showBackButton && (
           <ArrowLeft className="mr-4 cursor-pointer" onClick={navigateBack} />
